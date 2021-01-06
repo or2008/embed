ff7254a8464a044b0fccb9123997ffe3ecd1ce73
Or Bachar <or@creator.eco>
2021-01-06 15:05:45 +0200

    add link og tags support

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  entry: './src/index.js',
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('postcss-nested-ancestors'),
+                require('postcss-nested')
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+    library: 'Embed',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  }
+};
