5411e41063dd3c5e2802894df3812d651820082b
Or Bachar <>
2022-01-06 10:39:55 +0200

    fix fb

--- src/services.js
+++ src/services.js
@@ -163,7 +163,7 @@ export default {
   facebook: {
     regex: /https?:\/\/www.facebook.com\/([^\/\?\&]*)\/(.*)/,
     embedUrl: 'https://www.facebook.com/plugins/post.php?href=https://www.facebook.com/<%= remote_id %>&height=300',
-    html: "<iframe scrolling='no' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%; min-height: 250px; max-height: 1000px;'></iframe>",
+    html: "<iframe scrolling='no' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%; min-height: 350px; max-height: 1000px;'></iframe>",
     id: (ids) => {
       return ids.join('/');
     },
